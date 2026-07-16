e31ee7dc80a4fae8205ba3c617e3adca27fa5bb9
misba <misbahurairah@gmail.com>
2026-07-16 11:19:26 +0530

    Add files via upload

---
+++ minandmax.java
@@ -0,0 +1,17 @@
+public class minandmax{
+public static void main(String args[]){
+int a[] = {10,32,65,889};
+int min = a[0];
+int max = a[0];
+
+for(int i = 0; i<a.length;i++){
+if(a[i] < min)
+min = a[i];
+
+if(a[i] > max)
+max = a[i];
+}
+System.out.println(min);
+System.out.println(max);
+}
+}
