d37576631c7d3ff2fd205d423ec133209a8cc3a1
misba <misbahurairah@gmail.com>
2026-08-05 19:04:17 +0530

    Add array insertion functionality in InsertingArray.java
    Implement array insertion logic to add an element at a specified position.

---
+++ InsertingArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+      
+        int element = sc.nextInt();
+        int pos = sc.nextInt();
+        for (int i = n; i > pos; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[pos] = element;
+        for (int i = 0; i <= n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
